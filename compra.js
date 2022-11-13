e99eec0c7040dba20b2c40a578fee5f0b7674dd2
Noelia Andrada <noemoni11@gmail.com>
2022-11-13 18:48:52 -0300

    Tarjeta de datos de transferencia

--- compra.js
+++ compra.js
@@ -4,34 +4,39 @@ const inputCP = document.getElementById("inputCP");
 const inputDireccion = document.getElementById("inputDireccion");
 const inputEmail = document.getElementById("inputEmail");
 const transferencia = document.getElementById("transferencia");
-const tarjeta = document.getElementById("tarjeta");
 const btnTransferencia = document.getElementById("btnTransferencia");
-const btnTarjeta = document.getElementById("btnTarjeta");
+
+//Generar tarjeta con datos de transferencia
 
 function generarDatosTransferencia() {
     btnTransferencia.addEventListener("click", (e)=>{
+        e.preventDefault();
         if(!inputNombre.value||!inputCP.value||!inputDireccion.value||!inputEmail.value){
-            e.preventDefault();
             Toastify({
                 text: "Completá todos los datos",
-                duration: 3000
+                duration: 3000,
+                gravity: "bottom",
+                position: "left"
                 }).showToast();
         }else{
-            e.preventDefault();
             transferencia.innerHTML = "";
             const tarjetaTransferencia = document.createElement("div");
             tarjetaTransferencia.classList.add("col-xl-3","col-md-6","col-xs-12");
             tarjetaTransferencia.innerHTML= `<div class="card">
                                                 <h2>Datos de pago:</h2>
-                                                    <div>
-                                                        <span>CBU: 1111111111111111111111</span>
-                                                    <div>
-                                                        <span>Alias: alias.falso.falso</span>
-                                                    <div>
-                                                        <p>Una vez realizado el pago, envianos el comprobante a mail@falso.com,
-                                                        para comenzar el proceso de envío.</p>
-                                                    </div>
-                                            </div>`
+                                                <div class="p-2">
+                                                    <span>CBU: 1111111111111111111111</span>
+                                                </div>
+                                                <div class="p-2">
+                                                    <span>Alias: alias.falso.falso</span>
+                                                </div>
+                                                <div class="p-2">
+                                                    <p>Una vez realizado el pago, envianos el comprobante a mail@falso.com,
+                                                    para completar el proceso de compra y coordinar el envío.</p>
+                                                </div class="p-2">
+                                                <h2>¡Gracias por tu compra!</h2>
+                                            </div>
+                                            `
                 transferencia.appendChild(tarjetaTransferencia);
         }
     })
